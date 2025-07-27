e390a2b0faaf20d3e90082c23dc1642ce30b4b0d
chrisking1981 <48853558+chrisking1981@users.noreply.github.com>
2025-07-27 19:28:14 +0200

    Initial commit from master branch

---
+++ api/index.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Werkplekbeheer Almere</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
